8d704cc39690969ab88c2354b681d4d0783bb285
alishabab62 <alishabab62@gmail.com>
2023-01-18 12:02:55 +0530

    basic sign up form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Form from "./form";
+import React from "react";
 function App() {
+    let [name ,setName] =React.useState("");
+    let [email ,setEmail] =React.useState("");
+    let [username ,setUsername] =React.useState("");
+    let [password ,setPassword] =React.useState("");
+    
+        function handleName(e){
+            setName(e.target.value)
+        }
+        function handleEmail(e){
+            setEmail(e.target.value)
+        }
+        function handleUsername(e){
+            setUsername(e.target.value)
+        }
+        function handlePassword(e){
+            setPassword(e.target.value)
+        }
+        let inputStyle={
+          width:"80%",
+          height:"30px",
+          borderRadius:"5px",
+          outline:"none",
+          border:"none",
+          paddingLeft:"5px",
+          marginBottom:"25px"
+        }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex" , justifyContent:"center" , alignItems:"center" , backgroundColor:"gray" , height:"100vh"}}>
+      <div style={{ backgroundColor:"rgb(220, 122, 245)" , width:"30%" , display:"flex" , justifyContent:"center" , alignItems:"center" , flexDirection:"column" , borderRadius:"5px"}}>
+      <h3 style={{fontSize:"25px" , fontFamily:"sans-serif" , margin:"30px 30px"}}>Sign Up Form</h3>
+      <Form type={"text"} placeholder={"Username"}  value={name} style={inputStyle} fun={handleName}/>
+      <Form type={"text"} placeholder={"Name"}  value={username} style={inputStyle} fun={handleUsername}/>
+      <Form type={"email"} placeholder={"Email"}  value={email} style={inputStyle} fun={handleEmail}/>
+      <Form type={"password"} placeholder={"Password"}  value={password} style={inputStyle} fun={handlePassword}/>
+      <button style={inputStyle} >Submit</button>
+    </div>
     </div>
   );
 }
