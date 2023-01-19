d4628ef52d4b2307ea60a044d22e328d03269718
alishabab62 <alishabab62@gmail.com>
2023-01-19 12:51:42 +0530

    set state updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import Form from "./form";
 import React from "react";
 function App() {
-    let [name ,setName] =React.useState("");
-    let [email ,setEmail] =React.useState("");
-    let [username ,setUsername] =React.useState("");
-    let [password ,setPassword] =React.useState("");
+    let [inputs ,setInputs] =React.useState({});
     
-        function handleName(e){
-            setName(e.target.value)
+        function handleInputs(e){
+            setInputs({...inputs , [e.target.name]: e.target.value})
         }
-        function handleEmail(e){
-            setEmail(e.target.value)
-        }
-        function handleUsername(e){
-            setUsername(e.target.value)
-        }
-        function handlePassword(e){
-            setPassword(e.target.value)
-        }
+       
         let inputStyle={
           width:"80%",
           height:"30px",
@@ -33,10 +22,10 @@ function App() {
     <div style={{display:"flex" , justifyContent:"center" , alignItems:"center" , backgroundColor:"gray" , height:"100vh"}}>
       <div style={{ backgroundColor:"rgb(220, 122, 245)" , width:"30%" , display:"flex" , justifyContent:"center" , alignItems:"center" , flexDirection:"column" , borderRadius:"5px"}}>
       <h3 style={{fontSize:"25px" , fontFamily:"sans-serif" , margin:"30px 30px"}}>Sign Up Form</h3>
-      <Form type={"text"} placeholder={"Username"}  value={name} style={inputStyle} fun={handleName}/>
-      <Form type={"text"} placeholder={"Name"}  value={username} style={inputStyle} fun={handleUsername}/>
-      <Form type={"email"} placeholder={"Email"}  value={email} style={inputStyle} fun={handleEmail}/>
-      <Form type={"password"} placeholder={"Password"}  value={password} style={inputStyle} fun={handlePassword}/>
+      <Form type={"text"} placeholder={"Name"}  value={inputs.Name} style={inputStyle} name={"Name"} fun={handleInputs}/>
+      <Form type={"text"} placeholder={"Username"}  value={inputs.username} style={inputStyle} name={"username"} fun={handleInputs}/>
+      <Form type={"email"} placeholder={"Email"}  value={inputs.email} style={inputStyle} name={"email"} fun={handleInputs}/>
+      <Form type={"password"} placeholder={"Password"}  value={inputs.password} style={inputStyle} name={"password"} fun={handleInputs}/>
       <button style={inputStyle} >Submit</button>
     </div>
     </div>
